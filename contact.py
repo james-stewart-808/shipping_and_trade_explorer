66696bb77653c0ee3743b0c2e11c9c3079e4a93f
james-stewart-808 <jamesastewart1996@gmail.com>
2026-07-20 15:58:30 +0100

    Adding contact.py.

---
+++ contact.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import requests
+from io import StringIO
+import streamlit as st
+#st.markdown("##### Contact")
+st.subheader("Contact", divider = 'grey')
+
+st.write(
+"""
+UCL Shipping and Oceans Research Group
+
+UCL Energy Institute
+
+Central House
+
+14 Upper Woburn Place
+
+London WC1H 0NN
+
+shippingandoceans@ucl.ac.uk
+"""
+)
